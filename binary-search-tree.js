6cf850968ca60b3ef34890b7a85770b9d1a68d86
RSalcido2019 <ricardo.salcido@outlook.com>
2022-03-19 20:16:59 -0500

    search for min

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -38,4 +38,29 @@ class BST {
             return searchTree(node);
         }
     }
+    findMin() {
+        let current = this.root;
+        while (current.left !== null) {
+            current = current.left;
+        }
+        return current.data;
+    }
+    findMax() {
+        let current = this.root;
+        while (current.right !== null) {
+            current = current.right;
+        }
+        return current.data;
+    }
+    find(data) {
+        let current = this.root;
+        while (current.data !== data) {
+            if (data < current.data) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+    }
 }
+
